cbbc4d1fcf2dcaaeedb7a49572c848e59eb3112a
Herbert Fernandez Tamayo <hftamayo@gmail.com>
2020-11-08 07:11:36 -0600

    version 2 en desa1

--- propuestas/v01/training/train.py
+++ propuestas/v01/training/train.py
@@ -9,11 +9,8 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+
 #htamayo
-from azureml.core import Dataset, Datastore
-from azureml.data.datapath import DataPath
-
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -39,7 +36,27 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-#fin de clean_data
+#/htamayo
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+### YOUR CODE HERE ###
+#htamayo
+#v1
+web_path = [
+       'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+   ]
+
+ds = TabularDatasetFactory.from_delimited_files(path=web_path, separator=',')   
+x, y = clean_data(ds)
+#/v1
+
+# TODO: Split data into train and test sets.
+### YOUR CODE HERE ###a
+xds_train, xds_test, yds_train, yds_test = train_test_split(x, y, test_size = 0.3, random_state = 0)
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -57,35 +74,6 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-#fin de main    
-    
-
-#htamayo: poner todo esto dentro de una funcion
-
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-### YOUR CODE HERE ###
-#htamayo
-web_path = [
-       'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
-   ]
-
-ds = Dataset.Tabular.from_delimited_files(path=web_path, separator=',')   
-
-#htamayo
-clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-train_ds = ds;
-test_ds = ds;
-
-run = Run.get_context()
-
-#/htamayo    
 
 if __name__ == '__main__':
     main()
